81d727f53678c6465c33b947446a3cf4b0611238
hamberluo <hamberluo@gmail.com>
2019-06-17 15:56:34 +0800

    docs: update env setup

--- kotlinmaster/sample/app/src/main/java/org/kotlinmaster/MainActivity.kt
+++ kotlinmaster/sample/app/src/main/java/org/kotlinmaster/MainActivity.kt
@@ -1,18 +1,13 @@
 package org.kotlinmaster
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    var view: View? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        view?.setBackgroundColor(Color.RED)
     }
+
 }
