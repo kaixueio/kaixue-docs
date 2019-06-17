7fadde03bff3e852dc2829c3b71ed5985e356733
hamberluo <hamberluo@gmail.com>
2019-06-17 19:38:58 +0800

    docs: update null safe

--- kotlinmaster/sample/app/src/main/java/org/kotlinmaster/MainActivity.kt
+++ kotlinmaster/sample/app/src/main/java/org/kotlinmaster/MainActivity.kt
@@ -1,9 +1,12 @@
 package org.kotlinmaster
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var view: View = findViewById(R.id.tvContent)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
