559195fdb62f45dfa35a47afbfd4942db96e4583
hamberluo <hamberluo@gmail.com>
2019-07-23 20:20:46 +0800

    docs: add class check and cast section

--- kotlinmaster/sample/app/src/main/java/org/kotlinmaster/JavaSample.java
+++ kotlinmaster/sample/app/src/main/java/org/kotlinmaster/JavaSample.java
@@ -1,5 +1,14 @@
 package org.kotlinmaster;
 
+import android.app.Activity;
+
 public class JavaSample {
+
+    void main() {
+        Activity activity = new NewActivity();
+        if (activity instanceof NewActivity) {
+            ((NewActivity) activity).action();
+        }
+    }
 
 }
