d588aa2873794064281b1c074088868d3735ecf9
xcc3641 <446022570@qq.com>
2019-10-09 14:23:03 +0800

    Review: 挂起的本质

--- kotlinmaster/sample/app/src/main/java/org/kotlinmaster/MainActivity.kt
+++ kotlinmaster/sample/app/src/main/java/org/kotlinmaster/MainActivity.kt
@@ -1,7 +1,26 @@
 package org.kotlinmaster
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 abstract class MainActivity : AppCompatActivity() {
     protected abstract fun test()
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        GlobalScope.launch {
+            val a = suspendingGetImage(imageId)
+            avatarIv.setImageBitmap(image)
+        }
+    }
+
+    suspend fun suspendingGetImage(id: String) = withContext(Dispatchers.IO) {
+
+    }
 }
